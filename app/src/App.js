29bbfb647b1e98f60b1225e10520e4d7ab3a7b16
Dorian Morgan <doorain@gmail.com>
2017-10-27 12:11:46 -0600

    init set up and step one and two

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Calculator from './component/calculator/calculator.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+
+    );
+  }
+}
+
+export default App;
